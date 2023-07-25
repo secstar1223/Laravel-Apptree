0be16fad7e1674e00605553bfca2c5995f33f0e1
secstar1223 <secstar@gmail.com>
2023-07-24 22:36:38 -0700

    update session

--- database/migrations/2023_03_03_054620_create_sessions_table.php
+++ database/migrations/2023_03_03_054620_create_sessions_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
-            $table->integer('last_activity');
+            $table->integer('last_activity')->nullable();
         });
     }
 
