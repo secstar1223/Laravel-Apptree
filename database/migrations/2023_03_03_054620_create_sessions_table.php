e456d09406d9a0c96b440170c4618eb84d680322
secstar1223 <secstar@gmail.com>
2023-07-24 22:14:47 -0700

    update session table

--- database/migrations/2023_03_03_054620_create_sessions_table.php
+++ database/migrations/2023_03_03_054620_create_sessions_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
+            $table->string('id');
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
